60caacdbd3854c9d3c034d5c0de1092a38ba03fd
Jai Surya Senthilkumar <jaisurya.senthilkumar@integrate.com>
2024-08-08 21:11:30 +0530

    Initial Commit - Project Setup

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFiles: ["<rootDir>/jest.polyfills.ts"],
+};
---
+++ jest.setup.ts
@@ -0,0 +1,20 @@
+import { QueryCache } from "@tanstack/react-query";
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+
+const queryCache = new QueryCache();
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => {
+  server.resetHandlers();
+  queryCache.clear();
+});
+
+// Clean up after the tests are finished.
+afterAll(() => {
+  server.close();
+});
